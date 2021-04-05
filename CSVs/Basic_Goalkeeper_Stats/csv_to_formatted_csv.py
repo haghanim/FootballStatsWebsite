026cf0a461f17274fab245ef739c6187aa02e001
haghanim <markjhaghani@gmail.com>
2021-04-04 23:47:34 -0400

    Goalkeeper commits

--- CSVs/Basic_Goalkeeper_Stats/csv_to_formatted_csv.py
+++ CSVs/Basic_Goalkeeper_Stats/csv_to_formatted_csv.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
 
-df1 = pd.read_csv(r'/Users/markhaghani/Documents/GitHub/550FinalProject/CSVs/Basic_Goalkeeper_Stats/big5/2016_17.csv')
-df2 = pd.read_csv(r'/Users/markhaghani/Documents/GitHub/550FinalProject/CSVs/Basic_Goalkeeper_Stats/big5/2017_18.csv')
-df3 = pd.read_csv(r'/Users/markhaghani/Documents/GitHub/550FinalProject/CSVs/Basic_Goalkeeper_Stats/big5/2018_19.csv')
-df4 = pd.read_csv(r'/Users/markhaghani/Documents/GitHub/550FinalProject/CSVs/Basic_Goalkeeper_Stats/big5/2019_20.csv')
-df5 = pd.read_csv(r'/Users/markhaghani/Documents/GitHub/550FinalProject/CSVs/Basic_Goalkeeper_Stats/big5/2020_21.csv')
+df1 = pd.read_csv(r'/Users/markhaghani/Documents/GitHub/550FinalProject/CSVs/Basic_Goalkeeper_Stats/big5/2016_17.csv', header=1)
+df2 = pd.read_csv(r'/Users/markhaghani/Documents/GitHub/550FinalProject/CSVs/Basic_Goalkeeper_Stats/big5/2017_18.csv', header=1)
+df3 = pd.read_csv(r'/Users/markhaghani/Documents/GitHub/550FinalProject/CSVs/Basic_Goalkeeper_Stats/big5/2018_19.csv', header=1)
+df4 = pd.read_csv(r'/Users/markhaghani/Documents/GitHub/550FinalProject/CSVs/Basic_Goalkeeper_Stats/big5/2019_20.csv', header=1)
+df5 = pd.read_csv(r'/Users/markhaghani/Documents/GitHub/550FinalProject/CSVs/Basic_Goalkeeper_Stats/big5/2020_21.csv', header=1)
 
 df_ucl_1 = pd.read_csv(r'/Users/markhaghani/Documents/GitHub/550FinalProject/CSVs/Basic_Goalkeeper_Stats/ucl/2016_17.csv', header=1)
 df_ucl_2 = pd.read_csv(r'/Users/markhaghani/Documents/GitHub/550FinalProject/CSVs/Basic_Goalkeeper_Stats/ucl/2017_18.csv', header=1)
@@ -20,6 +20,12 @@ df_uel_4 = pd.read_csv(r'/Users/markhaghani/Documents/GitHub/550FinalProject/CSV
 df_uel_5 = pd.read_csv(r'/Users/markhaghani/Documents/GitHub/550FinalProject/CSVs/Basic_Goalkeeper_Stats/ucl/2020_21.csv', header=1)
 
 # Process names - remove "\"
+df1['Player'] = df1['Player'].apply(lambda x: x.split('\\')[0])
+df2['Player'] = df2['Player'].apply(lambda x: x.split('\\')[0])
+df3['Player'] = df3['Player'].apply(lambda x: x.split('\\')[0])
+df4['Player'] = df4['Player'].apply(lambda x: x.split('\\')[0])
+df5['Player'] = df5['Player'].apply(lambda x: x.split('\\')[0])
+
 df_ucl_1['Player'] = df_ucl_1['Player'].apply(lambda x: x.split('\\')[0])
 df_ucl_2['Player'] = df_ucl_2['Player'].apply(lambda x: x.split('\\')[0])
 df_ucl_3['Player'] = df_ucl_3['Player'].apply(lambda x: x.split('\\')[0])
@@ -38,7 +44,15 @@ df_ucl_1, df_ucl_2, df_ucl_3, df_ucl_4, df_ucl_5,
 df_uel_1, df_uel_2, df_uel_3, df_uel_4, df_uel_5] 
 
 master_df = pd.concat(dfs)
+
+# drop duplicates + 'rk' column
 master_df = master_df.drop_duplicates()
+drop_cols = ['Rk', 'Age', 'MP', 'Starts', 'Min', '90s', 'Matches']
+master_df.drop(columns=drop_cols, inplace=True)
+
+# rename columns
+master_df.columns = ['Player', 'Nation', 'Pos', 'Squad', 'Comp', 'Born', 'GA', 'GA_per_90', 'SoTA', 'Saves', 'save_percentage', 'wins',
+       'draws', 'losses', 'clean_sheet', 'clean_sheet_percentage', 'penalties_attempted', 'penalties_allowed', 'penalties_saved', 'penalties_missed', 'penalty_save_percentage']
 
 print(master_df)
 
