9c46cabc1f2bcb38eef05f257d1e2840406511d0
haghanim <markjhaghani@gmail.com>
2021-04-18 15:17:33 -0400

    Delete duplicate DB Config

--- hw2 files/server/routes/db-config.js
+++
@@ -1,8 +0,0 @@
-// Private. Will not be included in submission
-// Replace <Your Pennkey> and <Your Password>
-module.exports = {
-  host: "football-db.cu4awskiievq.us-east-1.rds.amazonaws.com",
-  user: "admin",
-  password: "messi123",
-  database: "football-db"
-};
