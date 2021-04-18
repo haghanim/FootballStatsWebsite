0fe877bad8c712d1ea78e351a8588a9b0ed168f6
yhl999 <yuanhan.li@protonmail.com>
2021-04-18 12:19:02 -0400

    HW2 front/backend files

---
+++ hw2 files/server/db-config.js
@@ -0,0 +1,8 @@
+// Private. Will not be included in submission
+// Replace <Your Pennkey> and <Your Password>
+module.exports = {
+  host: "football-db.cu4awskiievq.us-east-1.rds.amazonaws.com",
+  user: "admin",
+  password: "messi123",
+  database: "football-db"
+};
